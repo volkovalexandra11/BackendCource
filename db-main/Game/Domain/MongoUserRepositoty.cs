c32b523c71c450b791bf9f23f6c02edf310f2437
Alexandra Volkova <alexandr.sazhev@yandex.ru>
2021-11-22 23:28:04 +0500

    One more refactoring done

--- db-main/Game/Domain/MongoUserRepositoty.cs
+++ db-main/Game/Domain/MongoUserRepositoty.cs
@@ -35,7 +35,8 @@ namespace Game.Domain
 
         public UserEntity GetOrCreateByLogin(string login)
         {
-            var usersEntity = userCollection.Find(x => x.Login == login);
+            var usersEntity = userCollection
+                .Find(x => x.Login == login);
             if (usersEntity.CountDocuments() > 0)
             {
                 return usersEntity.First();
@@ -51,33 +52,30 @@ namespace Game.Domain
 
         public void Update(UserEntity user)
         {
-            //TODO: Ищи в документации ReplaceXXX
-            userCollection.ReplaceOne(new BsonDocument("_id", user.Id), user);
+            userCollection
+                .ReplaceOne(new BsonDocument("_id", user.Id), user);
         }
 
         public void Delete(Guid id)
         {
-            userCollection.DeleteOne(new BsonDocument("_id", id));
+            userCollection
+                .DeleteOne(new BsonDocument("_id", id));
         }
-
-        // Для вывода списка всех пользователей (упорядоченных по логину)
-        // страницы нумеруются с единицы
+        
         public PageList<UserEntity> GetPage(int pageNumber, int pageSize)
         {
-            //TODO: Тебе понадобятся SortBy, Skip и Limit
-            var a = userCollection
+            var userEntities = userCollection
                 .Find(new BsonDocument())
                 .SortBy(x => x.Login)
                 .Skip((pageNumber - 1) * pageSize)
                 .Limit(pageSize)
                 .ToList();
-            return new PageList<UserEntity>(a, 
+            return new PageList<UserEntity>(userEntities, 
                 userCollection.CountDocuments(new BsonDocument()), 
                 pageNumber, 
                 pageSize);
         }
-
-        // Не нужно реализовывать этот метод
+        
         public void UpdateOrInsert(UserEntity user, out bool isInserted)
         {
             throw new NotImplementedException();
