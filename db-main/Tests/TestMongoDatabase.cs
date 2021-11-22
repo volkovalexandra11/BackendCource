9b625fe8b94256c34892da844151331490b72be8
Alexandra Volkova <alexandr.sazhev@yandex.ru>
2021-11-22 22:07:56 +0500

    Created database

--- db-main/Tests/TestMongoDatabase.cs
+++ db-main/Tests/TestMongoDatabase.cs
@@ -7,8 +7,8 @@ namespace Tests
     {
         public static IMongoDatabase Create()
         {
-            var mongoConnectionString = Environment.GetEnvironmentVariable("mongodb+srv://kwcdb:kwcdb16112021@cluster0.zkvhx.mongodb.net/game-tests?retryWrites=true&w=majority")
-                                        ?? "mongodb://localhost:27017";
+            const string connectionStr = "mongodb+srv://volkovalexandra11:P2VRhLtb2J4FMAg@cluster0.1xzo0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+            var mongoConnectionString = Environment.GetEnvironmentVariable(connectionStr) ?? "mongodb://localhost:27017";
             var mongoClient = new MongoClient(mongoConnectionString);
             return mongoClient.GetDatabase("game-tests");
         }
